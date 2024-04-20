8be8c85bf13f3f79e2480335bb490cded8784d37
squall435 <167617940+squall435@users.noreply.github.com>
2024-04-20 21:23:49 +0200

    feat(menu): take user input

--- Menu.cs
+++ Menu.cs
@@ -2,12 +2,45 @@
 {
     public class Menu
     {
+        private int _option;
+
+        public int Option
+        {
+            get => _option;
+        }
+
+        public bool Exit
+        {
+            get => _option == 3;
+        }
+
         public void Display()
         {
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1. Students");
             Console.WriteLine("2. Subjects");
             Console.WriteLine("3. Exit\n");
+        }
+
+        public void Select()
+        {
+            string option;
+            bool isNumber;
+
+            Console.Write("Enter your choice: ");
+            option = Console.ReadLine() ?? "";
+            option = option.ToLower();
+            isNumber = int.TryParse(option, out _option);
+
+            if (!isNumber)
+                if ("students".Contains(option))
+                    _option = 1;
+                else if ("subjects".Contains(option))
+                    _option = 2;
+                else if ("exit".Contains(option))
+                    _option = 3;
+            if (_option <= 0 || _option > 3)
+                Console.WriteLine("Invalid input");
         }
     }
 }
