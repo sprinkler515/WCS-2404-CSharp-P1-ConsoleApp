40901050f0bf0b533d95f50b4ebfb36e2aa6a312
squall435 <167617940+squall435@users.noreply.github.com>
2024-04-21 17:32:17 +0200

    create campus class w/ display and select methods

---
+++ Campus.cs
@@ -0,0 +1,36 @@
+﻿namespace P1_AppConsole
+{
+    public class Campus
+    {
+        protected Dictionary<int, Student> Students = [];
+        protected Dictionary<int, Subject> Subjects = [];
+
+        protected void DisplayMenu(string[] menu)
+        {
+            foreach (string option in menu)
+                Console.WriteLine(option);
+            Console.WriteLine();
+        }
+        
+        protected int SelectMenu(string[] menu)
+        {
+            string choice;
+            bool isNumber;
+
+            Console.Write("Enter your choice: ");
+            choice = Console.ReadLine() ?? "";
+            choice = choice.ToLower();
+            isNumber = int.TryParse(choice, out int option);
+
+            if (!isNumber)
+                for (int i = 1; i <= menu.Length; i++)
+                    if (menu[i].Contains(choice))
+                        option = i;
+
+            if (option <= 0 || option > menu.Length)
+                Console.WriteLine("Invalid input");
+
+            return option;
+        }
+    }
+}
