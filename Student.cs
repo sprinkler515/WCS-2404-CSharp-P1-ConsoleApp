252ef462b2416829d13106307d231f52a4b005c0
squall435 <167617940+squall435@users.noreply.github.com>
2024-04-21 12:42:35 +0200

    feat(student): set new student id

--- Student.cs
+++ Student.cs
@@ -2,5 +2,26 @@
 {
     public class Student
     {
+        private int _id;
+
+        /* In progress:
+         * private string _firstName;
+         * private string _lastName;
+         * private double _age;
+         * private string _birthday;
+         * private List<double> _grades;
+         * private double _avgScore;
+        */
+
+        public void CreateNewStudent()
+        {
+            _id = SetId();
+        }
+
+        private int SetId()
+        {
+            var rand = new Random();
+            return rand.Next(1000, 9999);
+        }
     }
 }
