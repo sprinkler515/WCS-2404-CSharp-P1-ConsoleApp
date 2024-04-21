61a9f83b4bcf5a240da212e0ed5b1470fe734dc7
squall435 <167617940+squall435@users.noreply.github.com>
2024-04-21 16:24:11 +0200

    feat(menu): create display method

--- Student.cs
+++ Student.cs
@@ -23,5 +23,15 @@
             var rand = new Random();
             return rand.Next(1000, 9999);
         }
+
+        public void DisplayStudentMenu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. List students");
+            Console.WriteLine("2. Create student");
+            Console.WriteLine("3. Consult specific student");
+            Console.WriteLine("4. Add grade to a specific student");
+            Console.WriteLine("5. Back to menu");
+        }
     }
 }
