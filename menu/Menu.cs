8e0d22b2f1079209a6404a4a31ed8d1da6edd20b
squall435 <167617940+squall435@users.noreply.github.com>
2024-04-24 17:36:49 +0200

    feat(menu): rework

---
+++ menu/Menu.cs
@@ -0,0 +1,48 @@
+﻿namespace P1_AppConsole.menu
+{
+    public abstract class Menu
+    {
+        public List<string> Options;
+        public int Option;
+        public bool Exit;
+
+        public Menu()
+        {
+            Options = [];
+            Option = 0;
+            Exit = false;
+        }
+
+        public void Display()
+        {
+            foreach (string option in Options)
+                Console.WriteLine(option);
+            Console.WriteLine();
+        }
+
+        public void Select()
+        {
+            string select;
+            bool valid;
+
+            Console.Write("Enter your choice: ");
+            select = Console.ReadLine() ?? "Invalid input";
+            select = select.ToLower();
+            valid = int.TryParse(select, out Option);
+
+            if (!valid)
+                for (int i = 1; i <= Options.Count; i++)
+                {
+                    string s = Options[i - 1].ToLower();
+                    if (s.Contains(select) && s.Length >= 3)
+                        Option = i;
+                }
+            /*
+            if (Option <= 0 || Option > Options.Count)
+                Console.WriteLine("Invalid input");
+            */
+        }
+
+        public abstract void Selection();
+    }
+}
---
+++ menu/SubjectsMenu.cs
@@ -0,0 +1,32 @@
+﻿namespace P1_AppConsole.menu
+{
+    public class SubjectsMenu : Menu
+    {
+        public SubjectsMenu()
+        {
+            Options.Add("1. List subjects");
+            Options.Add("2. Add new subject");
+            Options.Add("3. Delete subject");
+            Options.Add("4. Back to menu");
+        }
+
+        public override void Selection()
+        {
+            while (!Exit)
+            {
+                switch (Option)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        Exit = true;
+                        break;
+                }
+            }
+        }
+    }
+}
